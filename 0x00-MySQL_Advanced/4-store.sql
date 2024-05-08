b5c0217b525ac27ad266f7271dab4a2c084e9fdc
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-08 13:06:21 -0700

    code for 4-store.sql

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+-- SQL script that creates a trigger that decreases the quantity
+-- of an item after adding a new order
+
+CREATE TRIGGER subtract_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+   UPDATE items
+   SET quantity = quantity - NEW.number
+   where name = NEW.item_name;
+END;

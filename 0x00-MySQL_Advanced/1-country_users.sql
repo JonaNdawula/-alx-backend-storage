7b3d79cccfa882d857a4754af8bd1b108bbd7b37
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-08 11:47:04 -0700

    code for 1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- An sql script creating table users 
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);

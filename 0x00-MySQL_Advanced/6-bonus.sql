be858c6eecfe56075a987d63c49921ccaf95972d
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-09 01:21:34 -0700

    code for 6-bonus.sql

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,14 @@
+-- An SQL script that creates a stored procedure 
+-- AddBonus which adds a new correction for a student
+
+DELIMITER//
+CREATE PROCEDURE AddBonus( IN user_id INT, IN project_name VARCHAR(255), IN score INT
+)
+BEGIN
+   INSERT INTO projects(name)
+   SELECT project_name WHERE
+   project_name NOT IN (SELECT name FROM projects);
+
+   INSERT INTO corrections  (user_id, project_id, score) VALUES (user_id, (SELECT id FROM projects WHERE name=project_name), score);
+END;//
+DELIMITER;//

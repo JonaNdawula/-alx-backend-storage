e8ef8de96ade213c0fc8cd436a0a5ff0fafc7eb5
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-09 01:31:42 -0700

    update

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -7,9 +7,9 @@ CREATE PROCEDURE AddBonus( IN user_id INT, IN project_name VARCHAR(255), IN scor
 )
 BEGIN
    INSERT INTO projects (name)
-   SELECT project_name WHERE
+   SELECT project_name FROM DUAL WHERE
    project_name NOT IN (SELECT name FROM projects);
 
    INSERT INTO corrections  (user_id, project_id, score) VALUES (user_id, (SELECT id FROM projects WHERE name=project_name), score);
-END;//
+END//
 DELIMITER;//

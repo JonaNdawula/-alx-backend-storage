86c3591b47ef74a0f58293c0451acf29138f37c1
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-09 01:00:34 -0700

    code for 5-valid_email.sql

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- An SQL script  that creates a trigger that resets the attribute
+-- valid_email only when the email has been changed
+
+DELIMETER //
+CREATE TRIGGER email_reset
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN 
+   IF OLD.email != New.email THEN
+       SET New.valid_email = 0;
+   END IF;
+END;//
+DELIMETER;

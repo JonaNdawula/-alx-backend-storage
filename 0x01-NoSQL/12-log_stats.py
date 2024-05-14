7eabcfb0b4546b396ef349cc81780791821a56b6
root <jonathan.ndawula@yahoo.com>
2024-05-14 02:56:39 -0700

    code for 12-log_stats.py

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Script using pymongo
+to get required statistics
+"""
+
+
+from pymongo import MongoClient
+
+
+client = MongoClient('localhost', 27017)
+
+db = client['logs']
+
+collection = db['nginx']
+
+total_logs = collection.count_documents({})
+
+methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+method_count = [collection.count_documents({'method': method})
+                for method in methods]
+
+status_get_count = collection.count_documents({'methods': 'GET',
+                                              'path': '/status'})
+
+print(f'{total_logs} logs')
+print('Methods:')
+for method, count in zip(methods, method_count):
+    print(f'\t{method}: {count}')
+print(f'GET /status: {status_get_count}')

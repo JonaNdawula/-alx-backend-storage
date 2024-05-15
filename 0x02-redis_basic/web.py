68a894a27ec62bd299ddfda64caa9fce75a99ee6
root <jonathan.ndawula@yahoo.com>
2024-05-15 05:02:49 -0700

    code for 5. Implementing an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+"""
+import functools
+import redis
+import requests
+from typing import Callable
+
+_redis = redis.Redis()
+
+
+def count_calls(method: Callable) -> callable:
+    @functools.wraps(method)
+    def wrapper(url: str) -> str:
+        key = f"count:{url}"
+        _redis.incr(key)
+        return method(url)
+    return wrapper
+
+
+def cache_result(method: Callable) -> Callable:
+    @functoools.wraps(url: str) -> str:
+        key = f"cache:{url}"
+        cached_result = _redis.get(key)
+        if cached_result is not None:
+            return cached_result.decode('utf-8')
+        result = method(url)
+        _redis.setex(key, 10, result)
+        return result
+    return wrapper
+
+
+@count_calls
+@cache_result
+def get_page(url: str) -> str:
+    response = requests.get(url)
+    return response.text

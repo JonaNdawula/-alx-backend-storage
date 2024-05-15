1065064b3bdcfec4798790312d0e673b3b0c3a37
root <jonathan.ndawula@yahoo.com>
2024-05-15 03:38:55 -0700

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,19 +18,21 @@ class Cache:
             data = data.encode('utf-8')
         elif isinstance(data, int) or isinstance(data, float):
             data = str(data).encode('utf-8')
+        elif isinstance(data, bytes):
+            pass
         self._redis.set(random_key, data)
         return random_key
 
     def get(self, key: str, func: Optional[Callable] = None):
         data = self._redis.get(key)
-        if data is not None:
-            data = data.decode('utf-8')
+        if data is not None and func:
+            data = func(data.decode('utf-8')) if func != int else func(int(data))
             if func:
                 data = func(data)
         return data
 
     def get_str(self, key: str) -> str:
-        return self.get(key, func=lambda x: x.decode('utf-8'))
+        return self.get(key, func=lambda x: x)
 
     def get_int(self, key: str) -> int:
         return self.get(key, func=int)
